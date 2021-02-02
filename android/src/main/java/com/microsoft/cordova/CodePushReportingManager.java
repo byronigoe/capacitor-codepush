f271b6889f64dc8d2a22b4599cccd30815749fe9
Heo Sangmin <heo@mapiacompany.com>
2021-02-02 10:27:43 +0900

    fix: use window.Capacitor.Plugins.CodePush in CodePushReportingManager

--- android/src/main/java/com/microsoft/cordova/CodePushReportingManager.java
+++ android/src/main/java/com/microsoft/cordova/CodePushReportingManager.java
@@ -27,10 +27,9 @@ public class CodePushReportingManager {
             return;
         }
 
-        // TODO: fix js call
         final String script = String.format(
             Locale.US,
-            "javascript:document.addEventListener(\"deviceready\", function () { window.codePush.reportStatus(%d, %s, %s, %s, %s, %s); });",
+            "javascript:document.addEventListener(\"deviceready\", function () { window.Capacitor.Plugins.CodePush.reportStatus(%d, %s, %s, %s, %s, %s); });",
             statusReport.status.getValue(),
             convertStringParameter(statusReport.label),
             convertStringParameter(statusReport.appVersion),
